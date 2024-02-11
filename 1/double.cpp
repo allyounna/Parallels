82cc3438aea162acefdc874bef3bf0ea63c0ec52
Allyounna <145887785+allyounna@users.noreply.github.com>
2024-02-11 19:11:24 +0700

    Add files via upload

---
+++ 1/double.cpp
@@ -0,0 +1,17 @@
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <cmath>
+
+double arr[10000000];
+
+int main()
+{
+    double s = 0, step = (M_PI * 2) / 10000000;
+    for (int i = 0; i < 10000000; i++)
+    {
+        arr[i] = sin(i * step);
+        s += arr[i];
+    }
+    std::cout << s << std::endl;
+}   
